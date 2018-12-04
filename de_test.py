d2763d3ac6de61159166d542a40b88d39e0238e6
hafidsb <hafidsriwijaya98@gmail.com>
2018-12-04 12:14:21 +0700

    minus stopping condition

--- de_test.py
+++ de_test.py
@@ -24,36 +24,46 @@ while hehe:
 
 population_list = []
 my_list = []
-cois = []
-donor_vector = [[]]
-trial_vector = [[]]
+donor_vector = []
+trial_vector = []
 
 for i in range(population):
-	population_list.append(round(random.uniform(lower_bound, upper_bound), 2))
+	population_list.append([round(random.uniform(lower_bound, upper_bound), 2), round(random.uniform(lower_bound, upper_bound), 2)])
 
 # every element in population undergoes mutation, recombination, and selection
-for i in range(1):
+for i in range(population):
+	print(population_list)
 	# mutation
+	cois = []	
+	self_val = population_list[i]
+
 	# assign r1, r2, r3
-	my_list = [x for x in population_list if x not in cois]
-	del my_list[i]
+	my_list = [x for x in population_list if x not in cois and x != self_val]
 	r1 =  random.choice(my_list)
 	cois.append(r1)
-	my_list = [x for x in population_list if x not in cois]
-	del my_list[i]
+	my_list = [x for x in population_list if x not in cois and x != self_val]
 	r2 =  random.choice(my_list)
-	cois.append(r2)
-	my_list = [x for x in population_list if x not in cois]
-	del my_list[i]
+	cois.append(r2)	
+	my_list = [x for x in population_list if x not in cois and x != self_val]
 	r3 =  random.choice(my_list)
 	cois.append(r3)
-	my_list = [x for x in population_list if x not in cois]
-	del my_list[i]
+	my_list = [x for x in population_list if x not in cois and x != self_val]
+
 	# calculate donor vector
-	donor_vector[i] = round(r1 + (0.5 *(r2 - r3)),2) # mutation factor(F) is 0.5
+	donor_vector.append([round(r1[0] + (0.5 *(r2[0] - r3[0])),2), round(r1[1] + (0.5 *(r2[1] - r3[1])),2)]) # mutation factor(F) is 0.5
 
 	# recombination
 	# calculate trial vector
-	trial_vector[i] = choice([donor_vector[i], population_list[i]], 1, 0.1) # CR is 0.1
+	
+	trial_vector_cois = choice(["donor_vector", "target_vector"], 1, 0.1) # CR is 0.1
+	if trial_vector_cois == "donor_vector":
+		trial_vector.append(donor_vector[i])
+	else:
+		trial_vector.append(population_list[i])
 
 	# selection
+	target_val = ackley(population_list[i][0], population_list[i][1])
+	trial_val = ackley(trial_vector[i][0], trial_vector[i][0])
+
+	if trial_val <= target_val:
+		population_list[i] = trial_vector[i]
